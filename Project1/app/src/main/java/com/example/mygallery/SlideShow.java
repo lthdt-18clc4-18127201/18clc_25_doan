8b179ccf172cdb7989b683db3c6883d0b435004d
lthdt-18clc4-18127201 <phamtansu2000@gmail.com>
2021-05-13 00:13:57 +0700

    Revert "Revert "SlideShow""
    This reverts commit 4ce46ccd5cc089060bf277720fa823b477ebdf0a.

--- Project1/app/src/main/java/com/example/mygallery/SlideShow.java
+++ Project1/app/src/main/java/com/example/mygallery/SlideShow.java
@@ -1,14 +1,40 @@
 package com.example.mygallery;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.ViewFlipper;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
 public class SlideShow extends AppCompatActivity {
     ViewFlipper viewFlipper;
+    static List<String> images;
+
     @Override
     protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.id);
+        setContentView(R.layout.slide_show);
+        viewFlipper = findViewById(R.id.v_flipper);
+        images = ImageGallery.listofallImages(this);
+        images.addAll(ImageGallery.listofallVideo(this));
+        for(int i = 0; i < images.size(); i++){
+            slideshownIMG(images.get(i));
+        }
+    }
+    public void slideshownIMG(String image){
+        ImageView imageView = new ImageView(this);
+        Glide.with(this)
+                .load(image)
+                .into(imageView);
+
+        viewFlipper.addView(imageView);
+        viewFlipper.setFlipInterval(4000);
+        viewFlipper.setAutoStart(true);
+        viewFlipper.setInAnimation(this, android.R.anim.slide_in_left);
+        viewFlipper.setOutAnimation(this, android.R.anim.slide_out_right);
+
     }
 }
