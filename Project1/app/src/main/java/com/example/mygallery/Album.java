c07783c6957f463f9ed48029895a1b6bb0a7fb7e
Minhgunter <hongminhau2011@gmail.com>
2021-05-17 00:54:17 +0700

    album

--- Project1/app/src/main/java/com/example/mygallery/Album.java
+++ Project1/app/src/main/java/com/example/mygallery/Album.java
@@ -2,21 +2,105 @@ package com.example.mygallery;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.bumptech.glide.Glide;
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Album extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private AlbumAdapter albumAdapter;
+    private List<Album> albumList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_album);
-        Intent i = getIntent();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        initCollapsingToolBar();
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        albumList = new ArrayList<>();
+        albumAdapter = new AlbumAdapter(this, albumList);
+
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.addItemDecoration(new GridSpacingItemDecoration());
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(albumAdapter);
+
+        PrepareAlbums();
+
+        try {
+            Glide.with(this).load(R.drawable.bf4).into((ImageView) findViewById(R.id.backdrop));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initCollapsingToolBar() {
+        final CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setTitle(" ");
+        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
+        appBarLayout.setExpanded(true);
+
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            boolean isShow = false;
+            int scrollRange = -1;
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (scrollRange == -1)
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                if (scrollRange + verticalOffset == 0) {
+                    collapsingToolbarLayout.setTitle("Cardview");
+                    isShow = true;
+                }else if (isShow) {
+                    collapsingToolbarLayout.setTitle(" ");
+                    isShow = false;
+                }
+            }
+        });
+    }
+
+
+    File dir = new File(path, "/appname/media/app images/");
+        if (!dir.isDirectory()) {
+            dir.mkdirs();
+        }
+
+        File file = new File(dir, filename + ".jpg");
+        String imagePath =  file.getAbsolutePath();
+        //scan the image so show up in album
+        MediaScannerConnection.scanFile(this,
+                new String[] { imagePath }, null,
+                new MediaScannerConnection.OnScanCompletedListener() {
+                    public void onScanCompleted(String path, Uri uri) {
+                        if(Config.LOG_DEBUG_ENABLED) {
+                            Log.d(Config.LOGTAG, "scanned : " + path);
+                        }
+                    }
+                });
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ Project1/app/src/main/java/com/example/mygallery/AlbumAdapter.java
@@ -0,0 +1,32 @@
+package com.example.mygallery;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class AlbumAdapter extends RecyclerView.Adapter {
+    public AlbumAdapter(Album album, List<Album> albumList) {
+    }
+
+    @NonNull
+    @NotNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
